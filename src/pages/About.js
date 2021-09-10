9919dcb8af90483c8f9ca5f17d56d08dd0ae803f
jcoatanea <julien.coatanea.dev@gmail.com>
2021-09-10 14:26:03 +0200

    mise en place navigation

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>À propos</h1>
+            <br />
+            <p>Reprehenderit cupidatat Lorem magna sunt ea cupidatat irure ullamco adipisicing
+                consectetur sunt magna. Nostrud elit cillum fugiat occaecat.
+                Nulla Lorem dolore nisi fugiat aliquip adipisicing.
+            </p>
+            <br />
+            <p>Reprehenderit cupidatat Lorem magna sunt ea cupidatat irure ullamco adipisicing
+                consectetur sunt magna. Nostrud elit cillum fugiat occaecat.
+                Nulla Lorem dolore nisi fugiat aliquip adipisicing.
+            </p>
+        </div>
+    );
+};
+
+export default About;
