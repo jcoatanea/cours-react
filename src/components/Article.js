04bf88ac9baa8ec36234b48771f85689c68859fb
jcoatanea <julien.coatanea.dev@gmail.com>
2021-09-16 17:14:33 +0200

    Mise en place du post et du formulaire dynamique

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,18 +4,30 @@ const Article = (props) => {
 
     const { article } = props;
 
+    const dateParser = (date) => {
+        let newDate = new Date(date).toLocaleDateString('fr-FR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+        })
+
+        return newDate;
+    }
+
     return (
         <div className="article">
             <div className="card-header">
                 <h3>{article.author}</h3>
-                <em>Posté le {article.date}]</em>
-                <p>{article.content}</p>
+                <em>Posté le {dateParser(article.date)}</em>
+            </div>
+            <p>{article.content}</p>
 
-                <div className="btn-container">
-                    <button>Edit</button>
-                    <button>Delete</button>
-                </div>
-
+            <div className="btn-container">
+                <button>Edit</button>
+                <button>Delete</button>
             </div>
 
         </div>
--- src/pages/News.js
+++ src/pages/News.js
@@ -6,7 +6,10 @@ import Article from '../components/Article';
 
 const News = () => {
 
-    const [newsData, setNewsData] = useState([])
+    const [newsData, setNewsData] = useState([]);
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("");
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         getData();
@@ -17,7 +20,27 @@ const News = () => {
         axios.get('http://localhost:3003/articles').then((res) => setNewsData(res.data));
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        if (content.length < 140) {
+            setError(true);
+        } else {
+            axios.post('http://localhost:3003/articles', {
+                author: author,
+                content: content,
+                date: Date.now(),
+            }).then(() => {
+                // On remet l'error sur false pour pas que la bordur rouge et le texte d'affiche apres avoir post
+                setError(false);
+                // Remettre le champs de texte vide dans le formulaire 
+                setAuthor("");
+                setContent("");
+                // Pour afficher dynamiquement le post que l'on vient de creer, pas oublier de rappeler la data
+                getData();
+            })
+        }
+    }
 
     return (
         <div className="news-container">
@@ -25,16 +48,22 @@ const News = () => {
             <Logo />
             <h1>News</h1>
 
-
-            <form>
-                <input type="text" placeholder="Nom" />
-                <textarea placeholder="Message"></textarea>
+            {/* On recupere l'evenement du handleSubmit en passant en parametre le e et en le recuperant dans le (e) */}
+            <form onSubmit={(e) => handleSubmit(e)}>
+                {/* value={author} pour vider le champs de texte du formulaire lié au .then  */}
+                <input type="text" placeholder="Nom" value={author} onChange={(e) => setAuthor(e.target.value)} />
+                <textarea style={{ border: error ? "1px solid red" : "1px solid #61dafb" }} placeholder="Message" value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+                {/* si error est true alors (&&) on affiche le texte */}
+                {error && <p>Veuillez ecrire min 140 caractères</p>}
                 <input type="submit" value="Envoyer" />
             </form>
+
             <ul>
-                {newsData.map((article) => (
-                    <Article article={article} key={article.id} />
-                ))}
+                {newsData
+                    .sort((a, b) => b.date - a.date)
+                    .map((article) => (
+                        <Article article={article} key={article.id} />
+                    ))}
             </ul>
         </div>
     );
